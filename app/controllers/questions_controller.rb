93b66515a864426dd9fd5b12c4384310aa6b66ea
yu shimamura <mashiyu0914@gmail.com>
2019-11-11 00:55:43 +0900

    add:エラー時のメッセージの追加
    演習１の完成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,23 @@
 class QuestionsController < ApplicationController
 
   def create
-    Question.create(name: params[:question][:name], content: params[:question][:content], email: params[:question][:email])  
-    redirect_to new_question_path, notice: "お問い合わせを送りました"
+    @question = Question.new(question_params)  
+    if @question.save 
+      redirect_to new_question_path, notice: "お問い合わせを送りました"
+    else
+      render "new"
+    end
   end
 
   def new
     @question = Question.new
+  end
+
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:name, :content,:email)
   end
 end
